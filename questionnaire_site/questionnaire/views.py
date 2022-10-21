03617b6fa7b263e78655b59c67cea0a4f7086152
paigni <ikontai4@gmail.com>
2022-10-21 20:22:27 +0600

    Добавлены новые функции

--- questionnaire_site/questionnaire/views.py
+++ questionnaire_site/questionnaire/views.py
@@ -1,3 +1,34 @@
+from django.http import HttpResponse
+from django.template import loader
 from django.shortcuts import render
 
-# Create your views here.
+from questionnaire.models import Question
+from questionnaire.forms import UserForm
+
+
+menu = [{'title': "О сайте", 'url_name': 'about'},
+        {'title': "Статистика опросов", 'url_name': 'statistic'}
+]
+
+def about(request):
+    return render(request, 'index.html', {'menu': menu, 'title': 'О сайте'})
+
+def statistic(request):
+    return render(request, 'index.html', {'menu': menu, 'title': 'Статистика опросов'})
+
+def succes(request):
+    return HttpResponse('Мы вас обязательно спросим о чём-то интересном позже!')
+
+def index(request):
+    if request.method == "POST":
+        latest_questionnaire = Question.objects.order_by('-pub_date')[:5]
+        template = loader.get_template('polls/index.html')
+        context = {
+            'latest_questionnaire': latest_questionnaire,
+        }
+        return HttpResponse(template.render(context, request))
+
+
+
+def success(request):
+   return HttpResponse('')
