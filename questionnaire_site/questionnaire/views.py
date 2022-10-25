0e2db93369162bf3ebdf2fb16a0e9226b2f737e4
paigni <ikontai4@gmail.com>
2022-10-25 20:42:15 +0600

    Добавлены новые классы

--- questionnaire_site/questionnaire/views.py
+++ questionnaire_site/questionnaire/views.py
@@ -1,34 +1,35 @@
 from django.http import HttpResponse
-from django.template import loader
 from django.shortcuts import render
-
-from questionnaire.models import Question
-from questionnaire.forms import UserForm
-
-
-menu = [{'title': "О сайте", 'url_name': 'about'},
-        {'title': "Статистика опросов", 'url_name': 'statistic'}
-]
-
-def about(request):
-    return render(request, 'index.html', {'menu': menu, 'title': 'О сайте'})
-
-def statistic(request):
-    return render(request, 'index.html', {'menu': menu, 'title': 'Статистика опросов'})
-
-def succes(request):
-    return HttpResponse('Мы вас обязательно спросим о чём-то интересном позже!')
-
-def index(request):
-    if request.method == "POST":
-        latest_questionnaire = Question.objects.order_by('-pub_date')[:5]
-        template = loader.get_template('polls/index.html')
-        context = {
-            'latest_questionnaire': latest_questionnaire,
-        }
-        return HttpResponse(template.render(context, request))
+from django.shortcuts import get_object_or_404, render
+from django.utils import timezone
+from django.views import generic
+from questionnaire.models import Question,Choice
 
 
+class IndexView(generic.ListView):
+    template_name = 'questionnaire/index.html'
+    context_object_name = 'latest_question_list'
 
-def success(request):
-   return HttpResponse('')
+    def get_queryset(self):
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'questionnaire/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('questionnaire:results', args=(question.id,)))
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'questionnaire/results.html', {'question': question})
+
