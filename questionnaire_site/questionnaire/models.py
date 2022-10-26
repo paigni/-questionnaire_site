ffaa217f7f5cd6c2961e8abaa0d0823a6942cf93
paigni <ikontai4@gmail.com>
2022-10-26 20:21:52 +0600

    Изменена модель

--- questionnaire_site/questionnaire/models.py
+++ questionnaire_site/questionnaire/models.py
@@ -3,8 +3,9 @@ from django.utils import timezone
 import datetime
 
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    question_title = models.CharField(max_length=200,verbose_name="Заголовок")
+    question_text = models.CharField(max_length=200,verbose_name="Вопрос")
+    pub_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
 
     def __str__(self):
         return self.question_text
@@ -15,7 +16,7 @@ class Question(models.Model):
 
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, null=True)
     choice_text = models.CharField(max_length=200)
 
     def __str__(self):
