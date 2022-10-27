b18f1060b157a0c2dcdc2a5ffb7906a9a1a56ccb
paigni <ikontai4@gmail.com>
2022-10-27 20:21:07 +0600

    Добавлена новая модель

--- questionnaire_site/questionnaire/models.py
+++ questionnaire_site/questionnaire/models.py
@@ -1,23 +1,22 @@
 from django.db import models
-from django.utils import timezone
-import datetime
 
 class Question(models.Model):
-    question_title = models.CharField(max_length=200,verbose_name="Заголовок")
-    question_text = models.CharField(max_length=200,verbose_name="Вопрос")
+    title = models.CharField(max_length=200,verbose_name="Заголовок")
+    text = models.CharField(max_length=200,verbose_name="Вопрос")
     pub_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
 
     def __str__(self):
-        return self.question_text
-
-    def was_published_recently(self):
-        now = timezone.now()
-        return now - datetime.timedelta(days=1) <= self.pub_date <= now
-
+        return self.text
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE, null=True)
-    choice_text = models.CharField(max_length=200)
+    text = models.CharField(max_length=200)
+    vote = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.choice_text
+        return self.text
+
+class Person(models.Model):
+    unic_user_id = models.CharField(max_lenght=200)
+    user_answer_to_ques = models.BoleanField(default=False)
+    user_answers = models.CharField(max_lenght=200)
