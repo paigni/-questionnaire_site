760e9e692e45153e897f55e6969d3f7a96a94565
paigni <ikontai4@gmail.com>
2022-10-27 20:21:37 +0600

    Частично переписан класс IndexView и функция vote

--- questionnaire_site/questionnaire/views.py
+++ questionnaire_site/questionnaire/views.py
@@ -2,8 +2,9 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
+
 from django.views import generic
-from questionnaire.models import Question,Choice
+from questionnaire.models import Question, Choice, Person
 
 
 class IndexView(generic.ListView):
@@ -11,25 +12,28 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.filter(
-            pub_date__lte=timezone.now()
-        ).order_by('-pub_date')[:5]
+        return Question.objects.order_by('-pub_date')[:1]
+
 
 def vote(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoesNotExist):
-        return render(request, 'questionnaire/detail.html', {
-            'question': question,
-            'error_message': "You didn't select a choice.",
-        })
+    if request.session.get('has_commented', False):
+        return HttpResponse("Мы Вас обязательно спросим о чём то интересном позже")
     else:
-        selected_choice.votes += 1
-        selected_choice.save()
-        return HttpResponseRedirect(reverse('questionnaire:results', args=(question.id,)))
+        question = get_object_or_404(Question, pk=question_id)
+        try:
+            selected_choice = question.choice_set.get(pk=request.POST['choice'])
+        except (KeyError, Choice.DoesNotExist):
+            return render(request, 'questionnaire/detail.html',
+                          {
+                              'question': question,
+                          })
+        else:
+            request.session['has_commented'] = True
+            selected_choice.votes += 1
+            selected_choice.save()
+            return HttpResponse('Спасибо ваш выбор учтён,вы можете просмотерть статитстику по ссылке')
+
 
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'questionnaire/results.html', {'question': question})
-
