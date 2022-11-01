aa35dfceacd3a574380cf4d4e7094f35824e99b6
paigni <ikontai4@gmail.com>
2022-11-01 20:20:22 +0600

    добалены сервисы

--- questionnaire_site/questionnaire/service.py
+++ questionnaire_site/questionnaire/service.py
@@ -1,2 +1,20 @@
-a = 123
-print(dir(a))
+from .models import Person
+
+
+def is_new_user(user):
+    people = Person.objects.filter(user_id=user)
+    if people.exists():
+        return False
+    else:
+        return True
+
+
+def is_user_answer(user, question):
+    people = Person.objects.filter(user_id=user, question_id=question)
+    if is_new_user(user):
+        return False
+    else:
+        if people.exists():
+            return True
+        else:
+            return False
