e550867afc2d7a9eedf41952136c897a906a1fa7
paigni <ikontai4@gmail.com>
2022-11-01 20:20:58 +0600

    переписана функция vote,модуль в разработке

--- questionnaire_site/questionnaire/views.py
+++ questionnaire_site/questionnaire/views.py
@@ -5,28 +5,29 @@ from django.utils import timezone
 
 from django.views import generic
 from questionnaire.models import Question, Choice, Person
+from .service import is_user_answer,is_new_user
 
 
 class IndexView(generic.ListView):
     template_name = 'questionnaire/index.html'
-    context_object_name = 'latest_question_list'
+    context_object_name = 'latest_question'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:1]
+        return Question.objects.latest('pub_date')
 
 
 def vote(request, question_id):
-    people = Person.objects.all()
     user = request.session.session_key
-    Person.user_id = user
     question = get_object_or_404(Question, pk=question_id)
-    if Person.user_id in people:
-        if question in Person:
-            return HttpResponse('index.html')
-        else:
+    if is_user_answer(user,question):
+        return HttpResponse('Мы вас обязательно спросим о чём то интересном позже')
+    else:
+        if is_new_user(user):
+            Person.user_id = user
+            Person.question = question
             selected_choice.votes += 1
             selected_choice.save()
-            return HttpResponse('Спасибо ваш выбор учтён,вы можете просмотерть статитстику по ссылке')
+        return HttpResponse('Спасибо ваш выбор учтён,вы можете просмотерть статитстику по ссылке')
 
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
