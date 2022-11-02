f451bd593893ac77f112bb68d661481ee6513272
paigni <ikontai4@gmail.com>
2022-11-02 20:20:08 +0600

    Добавлены тесты

--- questionnaire_site/questionnaire/tests.py
+++ questionnaire_site/questionnaire/tests.py
@@ -1,16 +1,31 @@
+import datetime
+
 from django.test import TestCase
-from .service import is_new_user, is_user_answer
+from questionnaire.service import is_user_answer
+from questionnaire.models import Person,Question
 
 
 class ServiceTest(TestCase):
 
-    def test_is_new_user(self):
-        user = 1311
-        future_user = is_new_user(user)
-        self.assertIs(future_user, True)
-
     def test_is_user_answer(self):
+        ques = Question()
+        ques.text = '131'
+        ques.title = '3131'
+        ques.pub_date = datetime.time()
+        ques.save()
+
+        people = Person()
+        people.user_id = 1311
+        people.question_id = 1
+        people.save()
+
         user = 1311
         question = 1
         future_answer = is_user_answer(user,question)
-        self.assertIs(future_answer, False)
+        self.assertTrue(future_answer)
+
+        user = 1312
+        question = 1
+        future_answer = is_user_answer(user, question)
+        self.assertFalse(future_answer)
+
