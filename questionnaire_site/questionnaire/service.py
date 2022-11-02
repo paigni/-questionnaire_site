df2bc42f4788c021cb16d0c19364afcc3d8bb898
paigni <ikontai4@gmail.com>
2022-11-02 20:19:58 +0600

    Удалена функция is_new_user

--- questionnaire_site/questionnaire/service.py
+++ questionnaire_site/questionnaire/service.py
@@ -1,20 +1,9 @@
-from .models import Person
-
-
-def is_new_user(user):
-    people = Person.objects.filter(user_id=user)
-    if people.exists():
-        return False
-    else:
-        return True
+from questionnaire.models import Person
 
 
 def is_user_answer(user, question):
     people = Person.objects.filter(user_id=user, question_id=question)
-    if is_new_user(user):
-        return False
+    if people.exists():
+        return True
     else:
-        if people.exists():
-            return True
-        else:
-            return False
+        return False
