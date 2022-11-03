0bc73e4c726cacc0e355f8b5a8fb533ca47647f5
paigni <ikontai4@gmail.com>
2022-11-03 20:16:51 +0600

    изменена функция QuestionAdmin

--- questionnaire_site/questionnaire/admin.py
+++ questionnaire_site/questionnaire/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from django import forms
 
-from .models import Choice, Question
+from questionnaire.models import Choice, Question
 
 
 class ChoiceInline(admin.StackedInline):
@@ -10,14 +9,15 @@ class ChoiceInline(admin.StackedInline):
 
 
 class QuestionAdmin(admin.ModelAdmin):
-    pub_date = forms.CharField
     class Meta:
         model = Question
-        fieldsets = [
-            (None, {'fields': ['question_title']}),
-            (None, {'fields': ['question_text']}),
-            ('Дата публикации', {'fields': ['pub_date']}),
+        fields = [
+            'title',
+            'text',
+            'pub_date'
         ]
+
     inlines = [ChoiceInline]
+
 
 admin.site.register(Question, QuestionAdmin)
