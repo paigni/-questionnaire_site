5e3eee022f5651b2d3dcf702af6d80166e3ff3d1
paigni <ikontai4@gmail.com>
2022-11-03 20:17:37 +0600

    изменения в пути

--- questionnaire_site/questionnaire/urls.py
+++ questionnaire_site/questionnaire/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 app_name = 'questionnaire'
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
+    path('', views.show_question_and_choice, name='index'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
