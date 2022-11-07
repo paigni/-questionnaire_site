fc5fa59952b76cf50be32b5459a143777b2650f1
paigni <ikontai4@gmail.com>
2022-11-07 20:19:49 +0600

    Рефакторинг show_question_and_choice,изменена логика работы vote,переписан results

--- questionnaire_site/questionnaire/views.py
+++ questionnaire_site/questionnaire/views.py
@@ -1,37 +1,40 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from django.shortcuts import get_object_or_404, render
-from django.utils import timezone
-
-from django.views import generic
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render, redirect
 from questionnaire.models import Question, Choice, Person
 from questionnaire.service import is_user_answer
 
 
 def show_question_and_choice(request):
-    template_name = 'questionnaire/index.html'
-    question = Question.objects.latest('pub_date')
-    latest_question_id = question.id
-    choices = Choice.objects.filter(question_id = latest_question_id )
-    return render(request, template_name, {'question':question, 'choices':choices})
-
-def vote(request):
     question = Question.objects.latest('pub_date')
     latest_question_id = question.id
     user = request.session.session_key
-    question = get_object_or_404(Question, pk=latest_question_id)
-    if is_user_answer(user,question):
+    if not is_user_answer(user, latest_question_id) and request.method == "GET":
+        template_name = 'questionnaire/index.html'
+        choices = Choice.objects.filter(question_id=latest_question_id )
+        return render(request, template_name, {'question': question, 'choices': choices})
+    else:
+        return HttpResponseRedirect('/vote/')
+
+
+def vote(request):
+    template_name = 'questionnaire/detail.html'
+    user = request.session.session_key
+    question = Question.objects.latest('pub_date')
+    latest_question_id = question.id
+    if is_user_answer(user, latest_question_id) and request.method == 'GET':
         return HttpResponse('Мы вас обязательно спросим о чём то интересном позже')
     else:
-        if request.method == 'POST':
-            people = Person()
-            people.user_id = user
-            people.question = question
-            people.save()
-            selected_choice.votes += 1
-            selected_choice.save()
-    return HttpResponse('Спасибо ваш выбор учтён,вы можете просмотерть статитстику по ссылке')
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+        people = Person()
+        people.user_id = user
+        people.question = question
+        people.save()
+        selected_choice.vote +=1
+        selected_choice.save()
+        return render(request,template_name)
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'questionnaire/results.html', {'question': question})
+
+def results(request):
+    template_name = 'questionnaire/results.html'
+    question = Question.objects.latest('pub_date')
+    return render(request, template_name, {'question': question})
