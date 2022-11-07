bbaada7fb3eaabb65b43b9cddcce9b03c73dafe4
paigni <ikontai4@gmail.com>
2022-11-07 20:20:43 +0600

    Правки pub_date

--- questionnaire_site/questionnaire/models.py
+++ questionnaire_site/questionnaire/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Question(models.Model):
     title = models.CharField(max_length=200,verbose_name="Заголовок")
     text = models.CharField(max_length=200,verbose_name="Вопрос")
-    pub_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации',editable=False)
+    pub_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
 
     def __str__(self):
         return self.text
